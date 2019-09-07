a0ae5aae073289e5221aa9e171d5fb6106f32d3b
MauricioSilv <mausilva828@gmail.com>
2019-09-07 11:43:38 -0300

    config background pages

---
+++ mobile/src/pages/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Background from '~/components/Background';
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Text>SignIn</Text>
+    </Background>
+  );
+}
