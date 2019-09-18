3cf8a0e42eb080e6ae47a7f3165e0e4b4709a60f
MauricioSilv <mausilva828@gmail.com>
2019-09-18 02:03:46 -0300

    Config styles subscription

---
+++ mobile/src/pages/Subscriptions/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  padding: 20px 0;
+`;
+
+export const MeetUpsList = styled.FlatList`
+  flex: 1;
+`;
